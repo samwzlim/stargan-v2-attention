0270c43d67278c3f4e91af19874469eac30ce5bf
Samuel Wei Ze Lim <74489037+samwzlim@users.noreply.github.com>
2024-10-22 17:09:47 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 """
 StarGAN v2
 Copyright (c) 2020-present NAVER Corp.
@@ -10,31 +12,32 @@ Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 
 FILE=$1
 
-if [ $FILE == "pretrained-network" ]; then
-    URL=https://www.dropbox.com/scl/fi/akxt03k63b7qb0g5ca69m/010000_nets_ema.ckpt?rlkey=i3pkqmsdsyfimwmnoo82dx7a5&st=o8g2lqgo&dl=0
+if [ "$FILE" == "pretrained-network" ]; then
+    URL="https://www.dropbox.com/scl/fi/akxt03k63b7qb0g5ca69m/010000_nets_ema.ckpt?rlkey=i3pkqmsdsyfimwmnoo82dx7a5&st=o8g2lqgo&dl=1"
     mkdir -p ./expr/checkpoints/celeba_hq
     OUT_FILE=./expr/checkpoints/celeba_hq/010000_nets_ema.ckpt
-    wget -N $URL -O $OUT_FILE
+    wget -N "$URL" -O "$OUT_FILE"
     
-elif  [ $FILE == "wing" ]; then
-    URL=https://www.dropbox.com/s/tjxpypwpt38926e/wing.ckpt?dl=0
+elif [ "$FILE" == "wing" ]; then
+    URL="https://www.dropbox.com/s/tjxpypwpt38926e/wing.ckpt?dl=1"
     mkdir -p ./expr/checkpoints/
     OUT_FILE=./expr/checkpoints/wing.ckpt
-    wget -N $URL -O $OUT_FILE
-    URL=https://www.dropbox.com/s/91fth49gyb7xksk/celeba_lm_mean.npz?dl=0
+    wget -N "$URL" -O "$OUT_FILE"
+    
+    URL="https://www.dropbox.com/s/91fth49gyb7xksk/celeba_lm_mean.npz?dl=1"
     OUT_FILE=./expr/checkpoints/celeba_lm_mean.npz
-    wget -N $URL -O $OUT_FILE
+    wget -N "$URL" -O "$OUT_FILE"
 
-elif  [ $FILE == "celeba-hq-dataset" ]; then
-    URL=https://www.dropbox.com/s/f7pvjij2xlpff59/celeba_hq.zip?dl=0
+elif [ "$FILE" == "celeba-hq-dataset" ]; then
+    URL="https://www.dropbox.com/s/f7pvjij2xlpff59/celeba_hq.zip?dl=1"
     ZIP_FILE=./data/celeba_hq.zip
     mkdir -p ./data
-    wget -N $URL -O $ZIP_FILE
-    unzip $ZIP_FILE -d ./data
-    rm $ZIP_FILE
+    wget -N "$URL" -O "$ZIP_FILE"
+    unzip "$ZIP_FILE" -d ./data
+    rm "$ZIP_FILE"
 
 else
-    echo "Available arguments are pretrained-network and celeba-hq-dataset."
+    echo "Available arguments are 'pretrained-network', 'wing', and 'celeba-hq-dataset'."
     exit 1
 
 fi
